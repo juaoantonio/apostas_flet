bfa222cd30310bb51ff60d4edbc2f9b120b7eb4f
Deloreto Melo <meloloretomarcelo@gmail.com>
2023-12-11 21:38:51 -0300

    adição dos icons do Flamengo e do Vasco

--- tela_confrontos.py
+++ tela_confrontos.py
@@ -2,11 +2,21 @@ import flet as ft
 import controles as c
 import tela_apostas
 
+
+img_fla = ft.Image(
+    src='apostas_flet/flamengo.png', 
+    fit= ft.ImageFit.CONTAIN,
+)
 flamengo = ft.Container(
 content= ft.Text(value='Flamengo')
 )
 placar_flamengo = ft.TextField(hint_text='0')
 txt = ft.Text(value=f'{placar_flamengo.value}')
+
+img_vasco = ft.Image(
+    src='apostas_flet/vasco.png',
+    fit= ft.ImageFit.CONTAIN,
+)
 
 def view():
     return ft.View(
@@ -14,6 +24,7 @@ def view():
         [
             ft.Row(
                 [
+                    img_fla,
                     flamengo,
                     placar_flamengo,
                     ft.Container(
@@ -22,7 +33,8 @@ def view():
                     ft.TextField(hint_text='0'),
                     ft.Container(
                         content= ft.Text(value= 'Vasco')
-                    )
+                    ),
+                    img_vasco,
                 ]
             ),
             ft.Row(
